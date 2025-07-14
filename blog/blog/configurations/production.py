0a522a205e2cd0fc520770c593e8972de524b93b
EduMendoza-2007 <mendozaeduardoomar@gmail.com>
2025-07-13 23:52:32 -0300

    corrección de errores tipográficos en la configuración de la base de datos de Django

--- blog/blog/configurations/production.py
+++ blog/blog/configurations/production.py
@@ -10,7 +10,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'midominio-production.com']
 #TODO: Configurar db para production
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlit3',
+        'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
         #en caso de usar una postgres usar:
         #'ENGINE': 'django.db.backends.postgresql',
