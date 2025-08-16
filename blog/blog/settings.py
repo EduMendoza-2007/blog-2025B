1ed60eb272c1bd228a967b8d048af75b8529d4a1
Manuel Rodriguez Serrano <manu.rodriguez.serrano@gmail.com>
2025-08-16 14:29:40 -0300

    config echa

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -67,7 +67,17 @@ WSGI_APPLICATION = "blog.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.getnev('DB_NAME'),
+
+        'USER': os.getenv('DB_USER'), 
+                
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+
+        'HOST': os.getenv('DB_HOST'),
+
+        'PORT': os.getenv('DB_PORT'), 
+
+        
     }
 }
 
